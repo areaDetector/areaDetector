f211462d68773d750ced00f4c23c076a23da38b7
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-12 22:01:59 -0500

    Changed bootswatch theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -146,7 +146,7 @@ html_theme_options = {
     # * sandstone (v3 only)
     # * united
     # * yeti (v3 only)
-    'bootswatch_theme': "united",
+    'bootswatch_theme': "flatly",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
