1988261780568d3f15ea0b8bb246256ecb09bb87
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-18 16:55:21 -0500

    Fix use of sed

--- documentation/correct_encoding.sh
+++ documentation/correct_encoding.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -o nounset
 FILE=$1
-(iconv -f utf-8 -t utf-8 $FILE > $FILE.conv) && (mv $FILE.conv $FILE)
+#(iconv -f utf-8 -t utf-8 $FILE > $FILE.conv) && (mv $FILE.conv $FILE)
+sed -i 's/[\d128-\d255]//g' $FILE
