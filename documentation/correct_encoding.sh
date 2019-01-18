faa0d32155b2af6d97fd1658fc832bab3c109c02
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-18 18:18:59 -0500

    Added converting from iso to utf-8

--- documentation/correct_encoding.sh
+++ documentation/correct_encoding.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o nounset
 FILE=$1
-#(iconv -f utf-8 -t utf-8 $FILE > $FILE.conv) && (mv $FILE.conv $FILE)
-sed -i 's/[\d128-\d255]//g' $FILE
+echo "Converting $FILE from iso-8859-1 to utf-8"
+(iconv -f iso-8859-1 -t utf-8 $FILE > $FILE.conv) && (mv $FILE.conv $FILE)
