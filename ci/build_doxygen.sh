ccb618fd6c75453c4100396b319c81d34f17c63a
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-19 14:04:37 -0500

    Try source build

--- ci/build_doxygen.sh
+++ ci/build_doxygen.sh
@@ -2,13 +2,16 @@
 
 DOXYGEN_VER=1.8.15
 DOXYGEN_TAR=${DOXYGEN_VER}.linux.bin.tar.gz
-DOXYGEN_URL="https://downloads.sourceforge.net/project/doxygen/rel-${DOXYGEN_VER}/doxygen-${DOXYGEN_VER}.linux.bin.tar.gz"
+DOXYGEN_URL="https://downloads.sourceforge.net/project/doxygen/rel-${DOXYGEN_VER}/doxygen-${DOXYGEN_VER}.src.tar.gz"
 TMP_DIR=${TMPDIR-/tmp}
 
 echo "Downloading from ${DOXYGEN_URL}"
 echo "Installing to ${TMP_DIR}"
 
-wget -O - "${DOXYGEN_URL}" | tar xz -C ${TMP_DIR} doxygen-${DOXYGEN_VER}/bin/doxygen
-export PATH="${TMP_DIR}/doxygen-${DOXYGEN_VER}/bin:$PATH"
+wget -O - "${DOXYGEN_URL}" | tar xvzf - -C ${TMP_DIR}
 
-doxygen --version
+cd ${TMP_DIR}/doxygen-${DOXYGEN_VER}
+mkdir build && cd build
+pwd
+
+cmake -G "Unix Makefiles" ..
