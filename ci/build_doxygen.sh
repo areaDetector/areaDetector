89f2455bd947e1445a79512a7029ad1826867479
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-19 13:51:38 -0500

    Try downloading from SF

--- ci/build_doxygen.sh
+++ ci/build_doxygen.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-DOXYGEN_VER=doxygen-1.8.15
+DOXYGEN_VER=1.8.15
 DOXYGEN_TAR=${DOXYGEN_VER}.linux.bin.tar.gz
-DOXYGEN_URL="http://doxygen.nl/files/${DOXYGEN_TAR}"
+DOXYGEN_URL="https://downloads.sourceforge.net/project/doxygen/rel-${DOXYGEN_VER}/doxygen-${DOXYGEN_VER}.linux.bin.tar.gz"
+TMP_DIR=${TMPDIR-/tmp}
 
-wget -O - "${DOXYGEN_URL}" | tar xz -C ${TMPDIR-/tmp} ${DOXYGEN_VER}/bin/doxygen
-export PATH="${TMPDIR-/tmp}/${DOXYGEN_VER}/bin:$PATH"
+echo "Downloading from ${DOXYGEN_URL}"
+echo "Installing to ${TMP_DIR}"
 
+wget -O - "${DOXYGEN_URL}" | tar xz -C ${TMP_DIR} doxygen-${DOXYGEN_VER}/bin/doxygen
+export PATH="${TMP_DIR}/doxygen-${DOXYGEN_VER}/bin:$PATH"
+
+doxygen --version
