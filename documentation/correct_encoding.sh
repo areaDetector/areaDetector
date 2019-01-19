31898cc5e35472738006a2589eae20550bc938fc
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-19 07:04:00 -0500

    Updated encoding conversion to result in ASCII

--- documentation/correct_encoding.sh
+++ documentation/correct_encoding.sh
@@ -3,3 +3,5 @@ set -o nounset
 FILE=$1
 echo "Converting $FILE from iso-8859-1 to utf-8"
 (iconv -f iso-8859-1 -t utf-8 $FILE > $FILE.conv) && (mv $FILE.conv $FILE)
+echo "Converting $FILE from utf-8 to ASCII"
+(iconv -f utf-8 -t ASCII//TRANSLIT $FILE > $FILE.conv) && (mv $FILE.conv $FILE)
