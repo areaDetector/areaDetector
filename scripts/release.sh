bb5d84e6ea6b2ddf532941755434d5e162b7c7f3
Henrique F. Simoes <henrique.simoes@lnls.br>
2024-03-13 12:33:29 -0300

    Generate release notes with sections
    The included sections are modules released, untagged updates and new
    modules. Commits are taken only from the diff range, so that it can also
    be used for generating releases from arbitrary commit ranges.
    
    Released modules are the ones that were git-tagged in the release commit
    range. Such modules have well-defined release notes that document the
    changes, which are linked.
    
    On the other hand, modules with untagged changes do not necessarily have
    partial release notes, so only their changeset is linked instead.
    
    New modules are listed in a separate section to make clear they weren't
    previously available. A direct link to the repository is provided along
    with release note links if applicable.
    
    A 'removed modules' section could be also created. However, an
    unexpected behavior of git-diff(1) is that it does not include removed
    submodules in its output. Thus, another approach should be taken for
    that.

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,30 +9,45 @@ fi
 
 github_url="https://github.com/areaDetector"
 
-# Find all tags after a given commit for a module
+
+# Find all tags in a given commit range (start_commit, end_commit] for a module
 get_module_releases() {
   module=$1
-  commit=$2
+  start_commit=$2
+  end_commit=$3
 
-  # Take commit right after the given one
-  first_commit=$(git -C $module log --format=%h --reverse $commit..HEAD | head -n 1)
+  # Take commit right after the first one
+  first_commit=$(git -C $module log --format=%h --reverse $start_commit..$end_commit | head -n 1)
 
-  tags="$(git -C $module tag --contains $first_commit)"
+  tags="$(git -C $module tag --contains $first_commit --no-contains $end_commit)"
+
+  # Include `end_commit` to the range
+  tags+=" $(git -C $module tag --points-at $end_commit)"
 
   echo $tags
 }
 
-# Print release information of each module based on the provided commit
+# Print release information of each module based on the provided commit range
+# and releases
 print_module_releases() {
   module=$1
-  commit=$2
+  start_commit=$2
+  end_commit=$3
+  releases="${@:4}"
 
-  read -ra tags <<< "$(get_module_releases $module $commit)"
+  read -ra tags <<< "$releases"
+
+  release_notes_url=$github_url/$module/blob/master/RELEASE.md
+  details_url=$github_url/$module
+
+  if [ "$start_commit" != "none" ]; then
+      details_url=$details_url/compare/$start_commit..$end_commit
+  fi
 
   if [ ${#tags[@]} -gt 0 ]; then
-    echo "* [$module ${tags[@]}]($github_url/$module/blob/master/RELEASE.md#${tags[-1],,})"
+    echo "* [$module]($details_url) [${tags[@]}]($release_notes_url#${tags[-1],,})"
   else
-    echo "* $module"
+    echo "* [$module]($details_url)"
   fi
 }
 
@@ -41,16 +56,61 @@ current_tag=$2
 
 changed_files=$(git diff --name-only $previous_tag..$current_tag)
 
-printf "### $current_tag\n\n"
+tagged=()
+untagged=()
+added=()
 
 for file in $changed_files; do
-  # Check if changed file is a submodule
-  tree_info=$(echo "$(git ls-tree $previous_tag)" | grep -oE ".*commit.*$file")
+  previous_tree=$(echo "$(git ls-tree $previous_tag)" | grep -oE ".*commit.*$file")
+  current_tree=$(echo "$(git ls-tree $current_tag)" | grep -oE ".*commit.*$file")
 
-  if [ -n "$tree_info" ]; then
-    module=$(echo $tree_info | cut -d' ' -f 4)
-    commit=$(echo $tree_info | cut -d' ' -f 3)
+  if [ -z "$previous_tree" -a -z "$current_tree" ]; then
+    # Changed file is not a submodule
+    continue
+  fi
 
-    print_module_releases $module $commit
+  start_commit=$(echo $previous_tree | cut -d' ' -f 3)
+  end_commit=$(echo $current_tree | cut -d' ' -f 3)
+  module=$(echo $current_tree | cut -d' ' -f 4)
+
+  if [ -z "$previous_tree" -a -n "$current_tree" ]; then
+    start_commit=$(git -C $module rev-list --max-parents=0 --reverse HEAD | head -n 1)
+    releases=$(get_module_releases $module $start_commit $end_commit)
+
+    added+=("$module none $end_commit $releases")
+  elif [ -n "$previous_tree" -a -n "$current_tree" ]; then
+    releases=$(get_module_releases $module $start_commit $end_commit)
+
+    if [ -n "$releases" ]; then
+      tagged+=("$module $start_commit $end_commit $releases")
+    else
+      untagged+=("$module $start_commit $end_commit")
+    fi
   fi
 done
+
+printf "### $current_tag\n\n"
+
+if [ ${#tagged[@]} -gt 0 ]; then
+  printf "#### Module releases\n\n"
+
+  for module_info in "${tagged[@]}"; do
+    print_module_releases $module_info
+  done
+fi
+
+if [ ${#untagged[@]} -gt 0 ]; then
+  printf "\n#### Modules with untagged updates\n\n"
+
+  for module_info in "${untagged[@]}"; do
+    print_module_releases $module_info
+  done
+fi
+
+if [ ${#added[@]} -gt 0 ]; then
+  printf "\n#### New modules\n\n"
+
+  for module_info in "${added[@]}"; do
+    print_module_releases $module_info
+  done
+fi
